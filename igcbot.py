2d04bd596d3f4445b8353bbf0e1ba9254b6231c9
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2018-08-27 07:05:51 +0000

    Update igcbot.py

--- igcbot.py
+++ igcbot.py
@@ -236,9 +236,9 @@ async def warn(ctx, userName: discord.User, *, message:str):
 @client.command(pass_context=True)
 async def ownerinfo(ctx):
     embed = discord.Embed(title="Information about owner", description="Bot Name- 【I.G.C】Bot", color=0x00ff00)
-    embed.set_footer(text="Copyright@UK Soft")
-    embed = site.Embed(title="                                   set_author(name=" Bot Owner Name-"|Sunny Singh|™✓#4856"-ID:"395535610548322326"
-     embed.add_field(name="Site- Will be made soon", value="Thanks for adding our bot", inline=True)
+    embed.set_footer(text="Copyright@UK Soft")                               
+    embed.set_author(name=" Bot Owner Name -@|Sunny Singh|™✓#4856 -ID:395535610548322326")
+    embed.add_field(name="Site- Will be made soon", value="Thanks for adding our bot", inline=True)
     await client.say(embed=embed)
     
 @client.command(pass_context = True)
